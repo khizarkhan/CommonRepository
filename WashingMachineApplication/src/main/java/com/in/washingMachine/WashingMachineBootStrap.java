da46964e6f91a4f8e1f431651899dd491c9c3866
khizarkhan <khankhizar4@gmail.com>
2020-12-22 02:44:46 -0500

    Add files via upload

---
+++ WashingMachineApplication/src/main/java/com/in/washingMachine/WashingMachineBootStrap.java
@@ -0,0 +1,14 @@
+package com.in.washingMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by khizar khan
+ */
+@SpringBootApplication
+public class WashingMachineBootStrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashingMachineBootStrap.class, args);
+	}
+}
